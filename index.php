12bf031fdeb30aa925e9b149655c71dbf28469e3
jakdev047 <jakfreelancer047@gmail.com>
2020-08-05 21:43:46 +0600

    initial setup

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>PHP</title>
+
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.0/milligram.css">
+
+      <style>
+         body{margin-top: 30px;}
+      </style>
+
+   </head>
+
+   <body>
+      <div class="container">
+         <div class="row">
+            <div class="column column-60 column-offset-20">
+               <h2>
+                  <?php echo "Hello World"; ?>
+               </h2>
+               <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero ipsa veritatis, nesciunt tempora ducimus fuga? Nobis iusto, iure cupiditate, deleniti labore ab repellendus modi consequatur nulla ex facilis quas quia adipisci itaque error ut, distinctio alias quae repellat totam! Provident impedit corporis excepturi. Beatae perferendis saepe, blanditiis qui consequuntur ipsam!
+               </p>
+            </div>
+         </div>
+      </div>
+   </body>
+
+</html>
